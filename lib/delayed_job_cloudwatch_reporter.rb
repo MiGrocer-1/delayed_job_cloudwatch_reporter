2d284f7e5de47348765ea948e2767eae27e47d9c
cicd <rushi@migrocer.in>
2023-04-25 17:34:01 +0530

    feat: use environment variable to enable disable reporting.

--- lib/delayed_job_cloudwatch_reporter.rb
+++ lib/delayed_job_cloudwatch_reporter.rb
@@ -2,4 +2,6 @@ module DelayedJobCloudwatchReporter
 end
 
 
-require 'delayed_job_cloudwatch_reporter/railties' if defined?(Rails::Railtie) && Rails::Railtie.respond_to?(:initializer)
+if defined?(Rails::Railtie) && Rails::Railtie.respond_to?(:initializer) && ENV.fetch("ENABLE_DELAYED_JOB_CLOUDWATCH_REPORTER") == 'true'
+  require 'delayed_job_cloudwatch_reporter/railties'
+end
