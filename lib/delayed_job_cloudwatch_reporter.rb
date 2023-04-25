18f399c2d684fa6c254ea483f334c199cdc6c74a
cicd <rushi@migrocer.in>
2023-04-25 18:52:41 +0530

    fix: don't crash if env variable is missing.

--- lib/delayed_job_cloudwatch_reporter.rb
+++ lib/delayed_job_cloudwatch_reporter.rb
@@ -2,6 +2,8 @@ module DelayedJobCloudwatchReporter
 end
 
 
-if defined?(Rails::Railtie) && Rails::Railtie.respond_to?(:initializer) && ENV.fetch("ENABLE_DELAYED_JOB_CLOUDWATCH_REPORTER") == 'true'
+if defined?(Rails::Railtie) && Rails::Railtie.respond_to?(:initializer) && ENV["ENABLE_DELAYED_JOB_CLOUDWATCH_REPORTER"] == 'true'
   require 'delayed_job_cloudwatch_reporter/railties'
+else
+  puts "delayed job cloudwatch reporter enabled: #{ENV["ENABLE_DELAYED_JOB_CLOUDWATCH_REPORTER"] || false}"
 end
